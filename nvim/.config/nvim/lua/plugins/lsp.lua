856f990449323751d92b12ac466aab263b79a0f7
Luke Davis <lukeda117@gmail.com>
2026-06-30 16:17:26 +0100

    Update lsp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,11 +1,17 @@
 return {
-    {
-        "neovim/nvim-lspconfig",
-    },
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
+	{
+		"neovim/nvim-lspconfig",
+	},
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = { "lua_ls", "clangd", "vue_ls", "ts_ls", "html" },
+		},
+	},
 }
